cc2f391bf4671ee987001275a42c6a9e50e6ecc4
CAM <cam_lee@me.com>
2020-09-15 22:20:43 +0800

    init project

---
+++ FizzBuzz/FizzBuzzUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzUtil
+    {
+        public static IEnumerable<string> GetFizzBuzzCollection(int count)
+        {
+            return count <= 0
+                ? throw new ArgumentOutOfRangeException($"The number {count} is out of range.")
+                : Enumerable.Range(1, count)
+                            .Select(num =>
+                                  $"{num}".Contains("3") && $"{num}".Contains("5") || num % 15 == 0 ? "FizzBuzz"
+                                : $"{num}".Contains("3") || num % 3 == 0 ? "Fizz"
+                                : $"{num}".Contains("5") || num % 5 == 0 ? "Buzz"
+                                : $"{num}"
+                            );
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var count = 100;    // default 100
+
+                if (args.Length == 1)
+                {
+                    int.TryParse(args[0], out count);
+                }
+
+                var result = FizzBuzzUtil.GetFizzBuzzCollection(count);
+                Console.WriteLine(string.Join(", ", result));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        
+    }
+}
